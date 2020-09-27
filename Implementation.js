4cba50717ac27bc75e91d36acb6e1420c7de8ab9
SoumenGhosh18 <soumentaki1996@gmail.com>
2020-09-27 11:54:13 +0530

    Added: Function to check for the option

--- Implementation.js
+++ Implementation.js
@@ -3,6 +3,9 @@ class SnakeLadder{
         var startPosition = 0;
         return (Math.floor(Math.random()*6)) + 1;
       }
+      checkForTheOption(){
+        return (Math.floor(Math.random() * 3)) +1;
+    }
 }
 
 module.exports = new SnakeLadder();
--- SnakeAndLadderMain.js
+++ SnakeAndLadderMain.js
@@ -1,3 +1,4 @@
 console.log("Welcome to Snake & Ladder Game");
 const result = require('./Implementation'); 
 console.log(result.generateRandomNo());
+console.log(result.checkForTheOption());
