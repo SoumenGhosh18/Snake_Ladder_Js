4967f56cf84913b9dcb1d29bf6ee86ab0e340512
SoumenGhosh18 <soumentaki1996@gmail.com>
2020-09-27 13:05:16 +0530

    Refactored : code to add some extra functionality

--- Implementation.js
+++ Implementation.js
@@ -1,11 +1,35 @@
+
 class SnakeLadder{
+   
     generateRandomNo(){
-        var startPosition = 0;
+       
         return (Math.floor(Math.random()*6)) + 1;
       }
       checkForTheOption(){
         return (Math.floor(Math.random() * 3)) +1;
     }
+    startPlayingFurther = () => {
+        let WinningPosition = 100;
+        let startPosition = 0;
+            let DiceNo= this.generateRandomNo();
+            let option = this.checkForTheOption();
+            switch (option) {
+                case 1:
+                    console.log("you got...." + DiceNo + " " + "so u r in front of snake") ;
+                    startPosition = startPosition - DiceNo
+                    console.log("now your position is..." + startPosition)
+                    break;
+
+                case 2:
+                    console.log("you got " + DiceNo + " " + "so u have to wait in same position because its no play");
+                   break;
+                case 3:
+                    console.log("you got...." + DiceNo + " " + "so u r in front of ladder") 
+                    startPosition = startPosition + DiceNo
+                    console.log("now your position is..." + startPosition);
+                    break;
+            }
+        }
 }
 
 module.exports = new SnakeLadder();
--- SnakeAndLadderMain.js
+++ SnakeAndLadderMain.js
@@ -2,3 +2,4 @@ console.log("Welcome to Snake & Ladder Game");
 const result = require('./Implementation'); 
 console.log(result.generateRandomNo());
 console.log(result.checkForTheOption());
+console.log(result.startPlayingFurther());
