6b26f09917c66c6cd20276e82ae9d01136997f95
SoumenGhosh18 <soumentaki1996@gmail.com>
2020-09-28 20:00:30 +0530

    Added : Code for playing the game between two player

--- Implementation.js
+++ Implementation.js
@@ -1,53 +1,68 @@
+let throwDice;
+let option;
+let turn = 0;
+let Player1Position = 0;
+let Player2Position = 0
 const WinningPosition = 100;
-let count =0;
-class SnakeLadder{
-   
-    generateRandomNo(){
-       
-        return (Math.floor(Math.random()*6)) + 1;
-      }
-      checkForTheOption(){
-        return (Math.floor(Math.random() * 3)) +1;
+let CountForFirstPlayer = 0;
+let CountForSecondPlayer = 0;
+
+class SnakeLadder {
+        generateRandomNo = (input) => {
+        return (Math.floor(Math.random() * input)) + 1;
     }
-    startPlayingFurther = () => {
+    startPlayingFurther (option, position) {
+        switch (option) {
+            case 1:
+                console.log("you got " + throwDice +" " + " You got a snake")
+                position = position - throwDice;
+                if (position < 0)
+                position = position + throwDice;
+                console.log("your current possition" + position)
+                break;
 
-        
-        let startPosition = 0;
-      
-        while (startPosition != WinningPosition) {
-   
-            let DiceNo= this.generateRandomNo();
-            let option = this.checkForTheOption();
-            switch (option) {
-                case 1:
-                    console.log("you got...." + DiceNo + " " + "so u r in front of snake") ;
-                    if (startPosition <0)
-                    {
-                        startPosition=0
-                    }
-                    else{
+            case 2:
+               console.log("you got " + throwDice +" " + "Sorry you have to stay here only");
+               console.log("Your position is...." + position)
+                break;
 
-                    startPosition = startPosition - DiceNo
-                    }
-                    console.log("now your position is..." + startPosition)
-                    count++;
-                    break;
+            case 3:
+               console.log("you got..." + throwDice +" "+ " you got a ladder");
+               position = position + throwDice;
+               if (position > WinningPosition) {
+                   position = position - throwDice;
+                   console.log("You cannot cross the limit " + WinningPosition)
+               }
+                   console.log("Your position is ..... " + position)
+                break;
+        }
+        return position;
+    }
+    playingBetweenTwoPlayer = () => {
 
-                case 2:
-                    console.log("you got " + DiceNo + " " + "so u have to wait in same position because its no play");
-                    count++;
-                   break;
-                case 3:
-                    console.log("you got...." + DiceNo + " " + "so u r in front of ladder") 
-                    startPosition = startPosition + DiceNo
-                    if(startPosition > WinningPosition)
-                     startPosition = startPosition - DiceNo;
-                    console.log("now your position is..." + startPosition);
-                    count++;
-                    break;
+        while (Player1Position != WinningPosition && Player2Position != WinningPosition) {
+            throwDice = this.generateRandomNo(6);
+            option = this.generateRandomNo(3);
+
+            if (turn % 2 == 0) {
+                console.log("This is the first player turn ")
+                Player1Position = this.startPlayingFurther(option, Player1Position);
+                CountForFirstPlayer++;
+                turn++;
+                if (Player1Position == WinningPosition) {
+                    console.log("Player 1 won the game...."+" "+"No of times dice played =>"+ CountForFirstPlayer);
+                }
+            }
+            else {
+                console.log("This is second player turn")
+                Player2Position = this.startPlayingFurther(option, Player2Position);
+                CountForSecondPlayer++;
+                turn++;
+                if (Player2Position == WinningPosition) {
+                    console.log("Player 2 won the game...."+" "+"No of times dice played =>"+ CountForSecondPlayer);
+                }
             }
         }
-        console.log("Number of times the dices rolled...."+count);
     }
 }
 module.exports = new SnakeLadder();
--- SnakeAndLadderMain.js
+++ SnakeAndLadderMain.js
@@ -1,5 +1,5 @@
 console.log("Welcome to Snake & Ladder Game");
 const result = require('./Implementation'); 
-console.log(result.generateRandomNo());
-console.log(result.checkForTheOption());
-console.log(result.startPlayingFurther());
+console.log(result.playingBetweenTwoPlayer());
+//console.log(result.checkForTheOption());
+//console.log(result.startPlayingFurther());
